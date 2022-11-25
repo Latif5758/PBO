bd1e87d2197ae2c1277641867e96c46d3e433982
Latif5758 <latifikhwanulmuttaqin@gmail.com>
2022-11-25 16:09:01 +0700

    1.2

--- connection.php
+++ connection.php
@@ -1,9 +1,5 @@
 <?php 
 	$conn = oci_connect("VOKER", "invokerjaya");
-	if (!$conn) {
-		echo "[failed] can not connect to database";
-		
-	} else {
-		// echo "[success] connected to database";
-	}
+	if (!$conn) echo "[failed] can not connect to database";
+	error_reporting(0);	
 ?>
