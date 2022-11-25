c083303b8213094b46279008ebc415e04475f88d
Latif5758 <latifikhwanulmuttaqin@gmail.com>
2022-11-25 16:14:40 +0700

    1.3

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 	<div class="row">
     <a class="col btn btn-primary" href="./mahasiswa/index.php">Data Mahasiswa</a>
     <br><br>
-    <a class="col btn btn-primary" href="./mahasiswa/index.php">Data Mata Kuliah</a>
+    <a class="col btn btn-primary" href="./mk/index.php">Data Mata Kuliah</a>
     <div>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
